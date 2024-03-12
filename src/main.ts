a7f795ccacd6e4e9bf57733ff1df0d1c03cf0f73
saqsun <sargsyan.sargis.89@gmail.com>
2024-03-12 11:39:53 +0400

    disable mixpanel debug

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 
 mixpanel.init("7f2f17de2932f13272e1f70f9301b2f5", {
-  debug: true,
+  debug: false,
   track_pageview: true,
   persistence: "localStorage",
 });
