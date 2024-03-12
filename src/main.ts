10bd639422430c01cf3499d59c43e83b27c4f127
saqsun <sargsyan.sargis.89@gmail.com>
2024-03-12 11:37:04 +0400

    added mixpanel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import mixpanel from "mixpanel-browser";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+mixpanel.init("7f2f17de2932f13272e1f70f9301b2f5", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 
 const app = createApp(App);
 
